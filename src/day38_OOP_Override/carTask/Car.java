3184e0aaa00f2c1c3329886de5200f500aa654ec
Joshua <98660305+JoshuaOsin@users.noreply.github.com>
2022-04-10 03:23:44 -0400

    Day38 Override

---
+++ src/day38_OOP_Override/carTask/Car.java
@@ -0,0 +1,26 @@
+package day38_OOP_Override.carTask;
+
+public class Car {
+
+    String brand, model;
+    int year;
+    double price;
+    String color;
+    long miles;
+
+    public Car(String brand, String model, int year, double price, String color, long miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
+
+    static{
+
+    }
+
+
+
+}
