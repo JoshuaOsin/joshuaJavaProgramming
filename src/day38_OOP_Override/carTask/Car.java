dc4af2b025bb829794b951a544fadcd3ce72081f
Joshua <98660305+JoshuaOsin@users.noreply.github.com>
2022-04-10 03:31:25 -0400

    Day38 Override

--- src/day38_OOP_Override/carTask/Car.java
+++ src/day38_OOP_Override/carTask/Car.java
@@ -2,11 +2,11 @@ package day38_OOP_Override.carTask;
 
 public class Car {
 
-    String brand, model;
-    int year;
-    double price;
-    String color;
-    long miles;
+    public String brand, model;
+    public int year;
+    public double price;
+    public String color;
+    public long miles;
 
     public Car(String brand, String model, int year, double price, String color, long miles) {
         this.brand = brand;
@@ -17,10 +17,22 @@ public class Car {
         this.miles = miles;
     }
 
-    static{
-
+    public void start(){
+        System.out.println(brand + " " + model + " is starting");
     }
 
+    public void drive(){
+        System.out.println(brand + " " + model + " is driving");
+    }
 
-
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
 }
