f6cbe1243f67a19c425d95ed4523faf372830376
Joshua <98660305+JoshuaOsin@users.noreply.github.com>
2022-04-30 05:04:02 -0400

    Day44 Abstract Interface Test

---
+++ src/day44_Abstraction_Interface/animalTask/Test.java
@@ -0,0 +1,15 @@
+package day44_Abstraction_Interface.animalTask;
+
+public class Test {
+    private int id;
+
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
